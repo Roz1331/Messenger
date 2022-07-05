f053ea04b5f72315458edce9c23a7e40aacb4fbe
Rose Ismailova <ismailova_roza_1@mail.ru>
2022-07-05 21:11:31 +0300

    sending messages properly

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,22 +1,14 @@
 
 
 
-function sendMes(userName) {
-    console.log("start sending message");
-    let messageText = $("#message").val();
-    if (messageText === "") return;
-    $.get('/message/send', { userName: userName, message: messageText})
-    .success(function() { console.log('Успешное выполнение'); })
-}
-
-// function getMessages(userName) {
-//     console.log("start getting messages for " + userName)
-//     $.get('/message/get', { userName: userName}, () => {
-//         console.log("messages were received");
+// function sendMes(userName) {
+//     let messageText = $("#message").val();
+//     console.log("start sending message: " + messageText);
+//     if (messageText === "") return;
+//     $.get('/message/send', { userName: userName, message: messageText}, function(data) {
+//         console.log(data); // ответ от сервера
 //     })
 // }
-//
-// setTimeout(getMessages, 1000, [[${userName}]]);
 
 
 // http://www.jetsource.ru/scripts/javascript_jquery/otpravka_post_i_get_zaprosov
