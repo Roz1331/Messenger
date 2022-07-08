75aa9978f76d70f1ca819b2da2b357e28b408322
Rose Ismailova <ismailova_roza_1@mail.ru>
2022-07-08 12:52:51 +0300

    external js file

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,15 +1,43 @@
 
+$(document).ready(function () {
 
+    $("#sendMessage").click(function () {
 
-// function sendMes(userName) {
-//     let messageText = $("#message").val();
-//     console.log("start sending message: " + messageText);
-//     if (messageText === "") return;
-//     $.get('/message/send', { userName: userName, message: messageText}, function(data) {
-//         console.log(data); // ответ от сервера
-//     })
-// }
+        let messageText = $("#message").val();
+        console.log("start sending message: " + messageText);
+        if (messageText === "") return; // if message is empty
+        $.get('/message/send', { userName: userName, message: messageText}, function(data) {
+            console.log("message was sent");
+            $("#message").val("")
+        })
+    })
 
+    setInterval(function () {
+        // let userName = [[${userName}]];
+        console.log("start getting messages for " + userName)
+        $.get('/message/get', { userName: userName}, function(data) {
+            data.forEach(function(element) {
+                console.log(element)
+                let messageSplit = element.split(separator)
+                let sender = messageSplit[0]
+                let textMessage = messageSplit.slice(1, messageSplit.length).join(separator)
 
+                let newMessage = document.createElement("p")
+                if (sender === userName) {
+                    newMessage.setAttribute("class", "right")
+                }
+                else {
+                    textMessage = sender + ": " + textMessage
+                    newMessage.setAttribute("class", "left")
+                }
+
+                let text = document.createTextNode(textMessage)
+                newMessage.appendChild(text)
+                let messagesList = document.getElementById("messages")
+                messagesList.appendChild(newMessage)
+            })
+        })
+    }, 2000);
+});
 // http://www.jetsource.ru/scripts/javascript_jquery/otpravka_post_i_get_zaprosov
 
